896ff03c248106c784f9ca499a46bf4f5009aeee
PinkD <443657547@qq.com>
2017-01-16 13:52:19 +0800

    add WHERE clause in cancel***.php

--- cancelExciting.php
+++ cancelExciting.php
@@ -15,18 +15,18 @@ $exciting = null;
 switch ($type) {
     case $TYPE_ANSWER:
         $sql = $pdo->prepare("DELETE FROM exciting_answer WHERE `uid` = ? AND `aid` = ?");
-        $exciting = $pdo->prepare("UPDATE question SET exciting = exciting - 1");
+        $exciting = $pdo->prepare("UPDATE question SET exciting = exciting - 1 WHERE id = ?");
         break;
     case $TYPE_QUESTION:
         $sql = $pdo->prepare("DELETE FROM exciting_question WHERE `uid` = ? AND `qid` = ?");
-        $exciting = $pdo->prepare("UPDATE question SET exciting = exciting - 1");
+        $exciting = $pdo->prepare("UPDATE question SET exciting = exciting - 1 WHERE id = ?");
         break;
     default:
         other_encode(400, "一点都不exciting");
 }
 
 if ($sql && $sql->execute(array($uid, $id))) {
-    $exciting->execute();
+    $exciting->execute($id);
     success_encode($info = "excited");
 } else {
     other_encode(500, "exciting失败");
--- cancelNaive.php
+++ cancelNaive.php
@@ -15,18 +15,18 @@ $naive = null;
 switch ($type) {
     case $TYPE_ANSWER:
         $sql = $pdo->prepare("DELETE FROM naive_answer WHERE `uid` = ? AND `aid` = ?");
-        $naive = $pdo->prepare("UPDATE question SET naive = naive - 1");
+        $naive = $pdo->prepare("UPDATE question SET naive = naive - 1 WHERE id = ?");
         break;
     case $TYPE_QUESTION:
         $sql = $pdo->prepare("DELETE FROM naive_question WHERE `uid` = ? AND `qid` = ?");
-        $naive = $pdo->prepare("UPDATE question SET naive = naive - 1");
+        $naive = $pdo->prepare("UPDATE question SET naive = naive - 1 WHERE id = ?");
         break;
     default:
         other_encode(400, "Too young!");
 }
 
 if ($sql && $sql->execute(array($uid, $id))) {
-    $naive->execute();
+    $naive->execute($id);
     success_encode($info = "naive");
 } else {
     other_encode(500, "Too simple!");
